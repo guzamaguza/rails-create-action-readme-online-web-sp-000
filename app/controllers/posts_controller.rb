ed644bab4ed638d303cf4ba1920fac4c82007e17
Mark A Guza <guzamaguza@gmail.com>
2020-07-14 05:07:14 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,12 @@ class PostsController < ApplicationController
 
   # add create method here
   def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
 
+    redirect_to post_path(@post)
   end
 
 end
