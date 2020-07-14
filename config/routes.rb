7f835c72e55e7c04bcd287b5aa409815cc24cb9d
Mark A Guza <guzamaguza@gmail.com>
2020-07-14 05:05:32 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts, only: [:]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/posts/new', to: 'posts#new', as: 'new_post'
+  get '/posts/:id', to: 'posts#show', as: 'post'
 end
